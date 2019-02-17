a14854cf4391e88a8c4b59b1618d15afe8a3b981
Trymunx <trymunx@gmail.com>
2019-02-17 21:53:39 +0000

    Make entering todos slightly easier
     - Focus the new todo input on page load
     - Add a submit button
     - Add description text to the 'Show Complete' button

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,13 +1,22 @@
 <template>
   <div class="todo-list">
-    <input type="text"
-      class="new-todo-input"
-      v-model="newTodoTitle"
-      placeholder="Enter new todo..."
-      @keypress.enter="addTodo">
-    <span class="toggle-complete-button fa-check-square"
-          :class="{'toggle-complete_show fas': displayComplete, far: !displayComplete}"
-          @click="toggleDisplayComplete"></span>
+    <div class="input-box-wrapper">
+      <input type="text"
+        class="new-todo-input"
+        v-model="newTodoTitle"
+        placeholder="Enter new todo..."
+        @keypress.enter="addTodo"
+        v-focus>
+      <i class="new-todo-input_submit fas fa-angle-right"
+        @click="addTodo"></i>
+      <div class="toggle-complete-button"
+            @click="toggleDisplayComplete">
+        <span>Show complete</span>
+        <i class="fa-check-square toggle-complete_checkbox"
+           :class="{'toggle-complete_show fas': displayComplete, far: !displayComplete}">
+        </i>
+      </div>
+    </div>
     <div v-if="loadingTodos" class="loading-message">Loading todos...</div>
     <div v-if="failedToLoad" class="failed-loading-message">Failed to load todos.</div>
     <div v-show="!loadingTodos && !failedToLoad">
@@ -69,6 +78,11 @@ export default Vue.extend({
         this.failedToLoad = true;
       });
   },
+  directives: {
+    focus: {
+      inserted: (el) => el.focus(),
+    },
+  },
 });
 </script>
 
@@ -78,10 +92,16 @@ export default Vue.extend({
   margin: 0 auto;
 }
 
+.input-box-wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 .new-todo-input {
   width: 80%;
   max-width: 380px;
   padding: 8px;
+  padding-right: 34px;
   margin: 12px;
   font-size: 1.2em;
   color: #505050;
@@ -95,18 +115,34 @@ export default Vue.extend({
   outline: none;
   box-shadow: 0px 2px 4px 1px #d4d4d4;
 }
+.new-todo-input_submit {
+  position: relative;
+  right: 38px;
+  color: #ccc;
+  font-size: 30px;
+  cursor: pointer;
+}
+.new-todo-input_submit:hover {
+  color: #9e9e9e;
+}
 
 .toggle-complete-button {
+  display: flex;
+  align-items: center;
   max-width: 80px;
-  font-size: 1.5rem;
+  font-size: 0.85rem;
   color: #aaa;
   background-color: #f0f0f0;
   border-radius: 10px;
-  padding: 5px 8px;
+  padding: 4px 8px;
   cursor: pointer;
 }
 .toggle-complete-button:hover {
   box-shadow: 0px 1px 3px 1px #cbcbcb;
+}
+.toggle-complete_checkbox {
+  font-size: 1.5rem;
+  margin: 6px;
 }
 .toggle-complete_show {
   color: #007529;
